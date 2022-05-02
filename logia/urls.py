0b4736d794b2802c94f2f26518f1f7f4510b707e
benilton02 <bsa.ele@uea.edu.br>
2022-05-02 02:38:14 -0300

    fix: token authentication

--- logia/urls.py
+++ logia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from logia import views
 
 urlpatterns = [
-    path('get-or-create-token/<str:username>', views.get_or_create_token),
+    path('create-user/', views.create_user),
     path('patients/', views.GetPatients.as_view()),
     path('pharmacies/', views.GetPharmacies.as_view()),
     path('transactions/', views.GetTransactions.as_view()),
--- logia/views.py
+++ logia/views.py
@@ -5,31 +5,45 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from logia.models import PatientsModel, PharmaciesModel, TransactionsModel, UsersModel, UsersModel
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 import json
 
-
+   
 @api_view(['POST'])
-def get_or_create_token(request, username):
-    try:
-        user = User.objects.get_or_create(id=1, username=username)
-        print(user)
-    except User.DoesNotExist:
-        data = {'error': 'User does not exist'}
-        return JsonResponse(data, status=404)
-    
-    token = list(Token.objects.get_or_create(user=user))
+def create_user(request):
+    payload = json.loads(request.body)
     data = {
-        "username": username,
-        "token": str(token[0])
-        }
-    return JsonResponse(data, status=200)
+        "id": payload.get("uuid"),
+        "username": payload.get("username"),
+        "password": payload.get("password")
+    }
+    
+    user = User.objects.get_or_create(**data)
+    token, created = Token.objects.get_or_create(user=user[0])
+    data = {
+        "token": token.pk,
+    }
+    return JsonResponse(data, status=201)
 
 
 class GetPatients(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     def get(self, request):
-        data = json.loads(request.body)
+        queryset = PatientsModel.objects.all()
+        patients = [
+            {
+                "uuid":query.uuid,
+                "first_name":query.first_name,
+                "last_name":query.last_name,
+                "date_of_birth":query.date_of_birth
+            }
+                for query in queryset
+        ]
+        data = {'patients': patients}
         return JsonResponse(data, status=200)
 
 
@@ -37,7 +51,16 @@ class GetPharmacies(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     def get(self, request):
-        data = json.loads(request.body)
+        queryset = PharmaciesModel.objects.all()
+        pharmacies =[
+            {
+                "uuid": query.uuid,
+                "name": query.name,
+                "city": query.city
+            }
+            for query in queryset
+        ]
+        data = {"pharmacies": pharmacies}
         return JsonResponse(data, status=200)
 
 
@@ -45,5 +68,21 @@ class GetTransactions(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     def get(self, request):
-        data = json.loads(request.body)
+        queryset = TransactionsModel.objects.all()
+        transaction =[
+            {
+                "patient_id": query.patient_uuid.uuid,
+                "first_name": query.patient_uuid.first_name,
+                "last_name": query.patient_uuid.last_name,
+                "date_of_birth": query.patient_uuid.date_of_birth,
+                "pharmacy_id": query.pharmacy_uuid.uuid,
+                "pharmacy_name": query.pharmacy_uuid.name,
+                "pharmacy_city": query.pharmacy_uuid.city,
+                "transaction_id": query.uuid,
+                "transaction_amount": query.amount,
+                "transaction_timestamp": query.timestamp
+            }
+            for query in queryset
+        ]
+        data = {"transaction":transaction}
         return JsonResponse(data, status=200)
